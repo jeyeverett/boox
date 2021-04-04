9e95518b3fd5c92c5c5d0b22f0eb4839b3eedee6
Jeysen <jeysenfreedman@gmail.com>
2021-04-03 22:05:02 -0400

    Fixed images on mobile.

--- seeds/index.js
+++ seeds/index.js
@@ -4,7 +4,9 @@ const images = require('./images');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/westcoastcamping', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+const dbURL = process.env.MONGO_URL || 'mongodb://localhost:27017/westcoastcamping';
+
+mongoose.connect('mongodb+srv://administrator:rq3SbLmHDXmnVLAH@coast2coastcamping.unfy8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
     .then(() => {
         console.log('Mongo connection established.');
     })
@@ -23,7 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20 + 10);
         const campground = new Campground({
-            author: "5fb4043c53896d28fc84110c",
+            author: "60691c552bda620015920b38",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dicta a aliquid nam aliquam consectetur ducimus quisquam, fuga reiciendis tenetur iure assumenda numquam placeat rerum. Voluptatum enim ab doloribus voluptas?',
