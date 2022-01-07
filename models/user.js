3fb58a219c0614f180cd38db504f1c0320f62439
Jeysen Freedman <jeysenfreedman@gmail.com>
2022-01-07 15:27:44 -0400

    updates

--- models/user.js
+++ models/user.js
@@ -20,7 +20,10 @@ const UserSchema = new Schema({
     unique: true,
   },
   profile: {
-    name: String,
+    name: {
+      type: String,
+      default: "New User",
+    },
     bio: String,
     images: [ImageSchema],
     inbox: [
